7ddd5ebb540d0fc19b45942ae488bb3f09d7fc29
rayGreycloud <rayGreycloud@gmail.com>
2017-08-26 08:51:17 -0700

    Set up style and script loading with enqueues

---
+++ wp-content/themes/udemy/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+// Setup
+
+
+// Includes
+include( get_template_directory() . '/includes/front/enqueue.php' );
+
+// Hooks
+add_action('wp_enqueue_scripts', 'ru_enqueue');
+
+// Shortcodes
+
+
+ ?>
---
+++ wp-content/themes/udemy/includes/front/enqueue.php
@@ -0,0 +1,7 @@
+<?php
+
+function ru_enqueue() {
+
+}
+
+ ?>
