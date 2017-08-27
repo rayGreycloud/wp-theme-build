06c77f97ce70e33b0d11b1e1fc0de77963896878
rayGreycloud <rayGreycloud@gmail.com>
2017-08-27 16:54:56 -0700

    Add Loop and thumbnail template tag

--- wp-content/themes/udemy/includes/setup.php
+++ wp-content/themes/udemy/includes/setup.php
@@ -2,6 +2,7 @@
 function ru_setup_theme() {
   // Unneeded except for older wp versions
   // add_theme_support( 'menus' );
+  add_theme_support( 'post-thumbnails' );
 
   register_nav_menu(
     'primary',
