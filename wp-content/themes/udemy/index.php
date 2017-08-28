6a0c38abf760a121ec66d221ca8fb640591b19d0
rayGreycloud <rayGreycloud@gmail.com>
2017-08-28 12:25:29 -0700

    Convert post fields using template tag functions

--- wp-content/themes/udemy/index.php
+++ wp-content/themes/udemy/index.php
@@ -41,23 +41,31 @@
 					?>
 
 	<div class="entry clearfix">
-		<div class="entry-image">
-			<a href="/images/blog/full/17.jpg" data-lightbox="image">
-			<?php the_post_thumbnail('full', array('class' => 'image_fade')); ?>
-			</a>
-		</div>
+  <?php
+
+  if(has_post_thumbnail()) {
+  ?>
+    <div class="entry-image">
+      <a href="<?php the_permalink(); ?>" data-lightbox="image">
+      <?php the_post_thumbnail('full', array('class' => 'image_fade')); ?>
+      </a>
+    </div>
+  <?php
+  }
+  ?>
+
 		<div class="entry-title">
-			<h2><a href="single.html">This is a Standard post with a Preview Image</a></h2>
+			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 		</div>
 		<ul class="entry-meta clearfix">
-			<li><i class="icon-calendar3"></i> 10th February 2014</li>
-			<li><a href="#"><i class="icon-user"></i> admin</a></li>
-			<li><i class="icon-folder-open"></i> <a href="#">General</a>, <a href="#">Media</a></li>
-			<li><a href="single.html#comments"><i class="icon-comments"></i> 13 Comments</a></li>
+			<li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+			<li><a href="<?php echo get_author_posts_url( get_the_author_meta('ID') ); ?>"><i class="icon-user"></i> <?php the_author(); ?></a></li>
+			<li><i class="icon-folder-open"></i> <?php the_category(' '); ?></li>
+			<li><a href="<?php the_permalink(); ?>#comments"><i class="icon-comments"></i> <?php comments_number('0'); ?> Comments</a></li>
 		</ul>
 		<div class="entry-content">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, asperiores quod est tenetur in. Eligendi, deserunt, blanditiis est quisquam doloribus voluptate id aperiam ea ipsum magni aut perspiciatis rem voluptatibus officia eos rerum deleniti quae nihil facilis repellat atque vitae voluptatem libero at eveniet veritatis ab facere.</p>
-			<a href="single.html" class="more-link">Read More</a>
+			<?php the_excerpt(); ?>
+			<a href="<?php the_permalink(); ?>" class="more-link">Read More</a>
 		</div>
 	</div>
 							<?php
