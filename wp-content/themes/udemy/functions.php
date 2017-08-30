9554eefdd8e4492d687b7517c40ee46c48622a97
rayGreycloud <rayGreycloud@gmail.com>
2017-08-30 12:47:37 -0700

    Set up custom control for facebook input

--- wp-content/themes/udemy/functions.php
+++ wp-content/themes/udemy/functions.php
@@ -7,11 +7,13 @@
 include( get_template_directory().'/includes/front/enqueue.php' );
 include( get_template_directory().'/includes/setup.php' );
 include( get_template_directory().'/includes/widgets.php' );
+include( get_template_directory().'/includes/theme-customizer.php' );
 
 // Hooks
 add_action('wp_enqueue_scripts', 'ru_enqueue');
 add_action('after_setup_theme', 'ru_setup_theme');
 add_action('widgets_init', 'ru_widgets');
+add_action('customize_register', 'ru_customize_register');
 
 // Shortcodes
 
