37494f611d9642b32c6dfc51525106060b46b55b
rayGreycloud <rayGreycloud@gmail.com>
2017-08-31 07:23:55 -0700

    Add settings and controls for copyright text andtos and privacy pages

--- wp-content/themes/udemy/includes/customizer/misc.php
+++ wp-content/themes/udemy/includes/customizer/misc.php
@@ -35,5 +35,50 @@ function ru_misc_customizer_section($wp_customize) {
       )
     )
   ));
+  $wp_customize->add_control( new WP_Customize_Control(
+    $wp_customize,
+    'ru_header_show_cart_input',
+    array(
+      'label'   => __('Show Shopping Cart in Header', 'udemy'),
+      'section' => 'ru_misc_section',
+      'settings'=> 'ru_header_show_cart',
+      'type'    => 'checkbox',
+      'choices' => array(
+        'yes'   => __('Yes', 'udemy')
+      )
+    )
+  ));
+
+  $wp_customize->add_control( new WP_Customize_Control(
+    $wp_customize,
+    'ru_footer_copyright_text_input',
+    array(
+      'label'   => __('Set Copyright Text', 'udemy'),
+      'section' => 'ru_misc_section',
+      'settings'=> 'ru_footer_copyright_text'
+    )
+  ));
+
+  $wp_customize->add_control( new WP_Customize_Control(
+    $wp_customize,
+    'ru_footer_tos_page_input',
+    array(
+      'label'   => __('TOS Page', 'udemy'),
+      'section' => 'ru_misc_section',
+      'settings'=> 'ru_footer_tos_page',
+      'type'    => 'dropdown-pages'
+    )
+  ));
+
+  $wp_customize->add_control( new WP_Customize_Control(
+    $wp_customize,
+    'ru_footer_privacy_page_input',
+    array(
+      'label'   => __('Privacy Policy Page', 'udemy'),
+      'section' => 'ru_misc_section',
+      'settings'=> 'ru_footer_privacy_page',
+      'type'    => 'dropdown-pages'
+    )
+  ));
 }
  ?>
