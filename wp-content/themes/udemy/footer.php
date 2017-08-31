e0ed18f45b1a79bb60e9b733d72fe940940b29dc
rayGreycloud <rayGreycloud@gmail.com>
2017-08-31 10:10:40 -0700

    Connect copyright, tos and privacy controls to tgemplate

--- wp-content/themes/udemy/footer.php
+++ wp-content/themes/udemy/footer.php
@@ -9,8 +9,20 @@
     <div class="container clearfix">
 
       <div class="col_half">
-        Copyrights &copy; 2017 All Rights Reserved by Jasko Koyn Inc.<br>
-        <div class="copyright-links"><a href="#">Terms of Use</a> / <a href="#">Privacy Policy</a></div>
+        <?php echo get_theme_mod('ru_footer_copyright_text') ?><br>
+        <div class="copyright-links">
+<?php
+
+if (get_theme_mod('ru_footer_tos_page')) {
+  ?><a href="<?php the_permalink(get_theme_mod('ru_footer_tos_page')); ?>">Terms of Use</a><?php
+}
+
+if (get_theme_mod('ru_footer_privacy_page')) {
+  ?><a href="<?php the_permalink(get_theme_mod('ru_footer_privacy_page')); ?>">Privacy Policy</a><?php
+}
+
+ ?>
+        </div>
       </div>
 
       <div class="col_half col_last tright">
