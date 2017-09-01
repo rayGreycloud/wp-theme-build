a1aa20fe17ac488641e143468cb32002b3ce653e
rayGreycloud <rayGreycloud@gmail.com>
2017-09-01 16:10:11 -0700

    Use post_class to output classes and set ID for posts

--- wp-content/themes/udemy/single.php
+++ wp-content/themes/udemy/single.php
@@ -1,13 +1,13 @@
 <?php get_header(); ?>
 
 <!-- Content ============================================= -->
-	<section id="content">
-		<div class="content-wrap">
-			<div class="container clearfix">
+<section id="content">
+	<div class="content-wrap">
+		<div class="container clearfix">
 
-				<!-- Post Content
-        ============================================= -->
-				<div class="postcontent nobottommargin clearfix">
+<!-- Post Content
+============================================= -->
+			<div class="postcontent nobottommargin clearfix">
 
 
 <?php
@@ -19,37 +19,38 @@ if( have_posts() ) {
 		$author_url = get_author_posts_url($author_ID);
 ?>
 
-					<div class="single-post nobottommargin">
+				<div id="post-<?php the_ID(); ?>"
+					<?php post_class('single-post nobottommargin'); ?>>
 
 <!-- Single Post
 ============================================= -->
-						<div class="entry clearfix">
+					<div class="entry clearfix">
 
 <!-- Entry Title
 ============================================= -->
-							<div class="entry-title">
-								<h2><?php the_title(); ?></h2>
-							</div><!-- .entry-title end -->
+						<div class="entry-title">
+							<h2><?php the_title(); ?></h2>
+						</div><!-- .entry-title end -->
 
 <!-- Entry Meta
 ============================================= -->
-							<ul class="entry-meta clearfix">
-								<li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
-								<li><a href="<?php echo $author_url; ?>"><i class="icon-user"></i> <?php the_author(); ?></a></li>
-								<li><i class="icon-folder-open"></i> <?php the_category(' '); ?></li>
-								<li><a href="<?php the_permalink(); ?>#comments"><i class="icon-comments"></i> <?php comments_number('0'); ?> Comments</a></li>
-							</ul><!-- .entry-meta end -->
+						<ul class="entry-meta clearfix">
+							<li><i class="icon-calendar3"></i> <?php echo get_the_date(); ?></li>
+							<li><a href="<?php echo $author_url; ?>"><i class="icon-user"></i> <?php the_author(); ?></a></li>
+							<li><i class="icon-folder-open"></i> <?php the_category(' '); ?></li>
+							<li><a href="<?php the_permalink(); ?>#comments"><i class="icon-comments"></i> <?php comments_number('0'); ?> Comments</a></li>
+						</ul><!-- .entry-meta end -->
 
 	<!-- Entry Image
 	============================================= -->
 		<?php
 		if(has_post_thumbnail()) {
 		?>
-					    <div class="entry-image">
-					      <a href="<?php the_permalink(); ?>">
-					      <?php the_post_thumbnail('full'); ?>
-					      </a>
-					    </div>
+				    <div class="entry-image">
+				      <a href="<?php the_permalink(); ?>">
+				      <?php the_post_thumbnail('full'); ?>
+				      </a>
+				    </div>
 		<?php
 		}
 		?>
@@ -57,62 +58,62 @@ if( have_posts() ) {
 
 <!-- Entry Content
 ============================================= -->
-							<div class="entry-content notopmargin">
-							<?php
-							the_content();
-							wp_link_pages(array(
-								'before' => '<p class="text-center">' . __( 'Pages:' ),
-								'after' => '</p>'
-							));
-							?>
+						<div class="entry-content notopmargin">
+<?php
+the_content();
+wp_link_pages(array(
+	'before' => '<p class="text-center">' . __( 'Pages:' ),
+	'after' => '</p>'
+));
+?>
 
-	<!-- Post Single - Content End -->
+<!-- Post Single - Content End -->
 
-	<!-- Tag Cloud
-	============================================= -->
-								<div class="tagcloud clearfix bottommargin">
-								<?php the_tags('', ' '); ?>
-								</div><!-- .tagcloud end -->
+<!-- Tag Cloud
+============================================= -->
+							<div class="tagcloud clearfix bottommargin">
+							<?php the_tags('', ' '); ?>
+							</div><!-- .tagcloud end -->
 
-								<div class="clear"></div>
+							<div class="clear"></div>
 
-							</div>
-						</div><!-- .entry end -->
+						</div>
+					</div><!-- .entry end -->
 
 <!-- Post Navigation
 ============================================= -->
-						<div class="post-navigation clearfix">
+					<div class="post-navigation clearfix">
 
-							<div class="col_half nobottommargin">
-								<?php previous_post_link(); ?>
-							</div>
+						<div class="col_half nobottommargin">
+							<?php previous_post_link(); ?>
+						</div>
 
-							<div class="col_half col_last tright nobottommargin">
-								<?php next_post_link(); ?>
-							</div>
+						<div class="col_half col_last tright nobottommargin">
+							<?php next_post_link(); ?>
+						</div>
 
-						</div><!-- .post-navigation end -->
+					</div><!-- .post-navigation end -->
 
-						<div class="line"></div>
+					<div class="line"></div>
 
 <!-- Post Author Info
 ============================================= -->
-							<div class="panel panel-default">
-							<div class="panel-heading">
-								<h3 class="panel-title">Posted by <span><a href="<?php echo $author_url; ?>"><?php the_author(); ?></a></span></h3>
-							</div>
-								<div class="panel-body">
-									<div class="author-image">
-										<?php echo get_avatar($author_ID, 90, '', false, array('class' => 'img-circle')); ?>
-									</div>
-									<?php echo nl2br(get_the_author_meta('description')); ?>
+						<div class="panel panel-default">
+						<div class="panel-heading">
+							<h3 class="panel-title">Posted by <span><a href="<?php echo $author_url; ?>"><?php the_author(); ?></a></span></h3>
+						</div>
+							<div class="panel-body">
+								<div class="author-image">
+									<?php echo get_avatar($author_ID, 90, '', false, array('class' => 'img-circle')); ?>
 								</div>
-							</div><!-- Post Single - Author End -->
+								<?php echo nl2br(get_the_author_meta('description')); ?>
+							</div>
+						</div><!-- Post Single - Author End -->
 
-							<div class="line"></div>
+						<div class="line"></div>
 
-							<h4>Related Posts:</h4>
-								<div class="related-posts clearfix">
+						<h4>Related Posts:</h4>
+							<div class="related-posts clearfix">
 <?php
 
 $categories = get_the_category();
