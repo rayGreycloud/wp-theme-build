002e4c83d14ca1d2996cc3515f62b7c20f5f2bbb
rayGreycloud <rayGreycloud@gmail.com>
2017-09-04 10:30:45 -0700

    Set up recipe plugin and add header properties

---
+++ wp-content/plugins/recipe/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Plugin Name: Recipe
+ * Description: A simple WordPress plugin that allows users to create and rate recipes
+ * Version: 1.0
+ * Author: rayGreycloud
+ * Author URI: http://rayGreycloud.com
+ * Text Domain: recipe
+ */
+
+ ?>
