d4acfdbeb0bd5e04a7cc71e4905aad3b9ff5a300
rayGreycloud <rayGreycloud@gmail.com>
2017-12-18 13:35:41 -0800

    Add custom post type for recipe

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -18,7 +18,10 @@ if (!function_exists('add_action')) {
 
 // Includes
 include('includes/activate.php');
+include('includes/init.php');
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
+add_action('init', 'recipe_init');
+
  ?>
