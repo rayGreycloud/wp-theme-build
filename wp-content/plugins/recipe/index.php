a6b4ddcf2bf20087696f6e489b1d520639358b96
rayGreycloud <rayGreycloud@gmail.com>
2017-12-20 10:21:48 -0800

    Add recipe options, save post process and form metadata

--- wp-content/plugins/recipe/index.php
+++ wp-content/plugins/recipe/index.php
@@ -20,10 +20,12 @@ define('RECIPE_PLUGIN_URL', __FILE__);
 include('includes/activate.php');
 include('includes/init.php');
 include('includes/admin/init.php');
+include('process/save-post.php');
 
 // Hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
 add_action('init', 'recipe_init');
 add_action('admin_init', 'recipe_admin_init');
+add_action('save_post_recipe', 'r_save_post_admin', 10, 3);
 
  ?>
