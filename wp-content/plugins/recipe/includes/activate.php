4659e177489f6594d71da5b8aa5830e6c90429c9
rayGreycloud <rayGreycloud@gmail.com>
2017-12-20 17:35:53 -0800

    Create table for recipe-ratings

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -5,5 +5,20 @@ function r_activate_plugin() {
   if (version_compare(get_bloginfo('version'), '4.5', '<')) {
     wp_die(__('You must update WordPress to use this plugin', 'recipe'));
   }
+  
+  global $wpdb;
+  
+  $createSQL = "
+    CREATE TABLE `" . $wpdb->prefix . "recipe_ratings` (
+    	`ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+    	`recipe_id` BIGINT(20) UNSIGNED NOT NULL,
+    	`rating` FLOAT(3,2) UNSIGNED NOT NULL,
+    	`user_ip` VARCHAR(32) NOT NULL,
+	     PRIMARY KEY (`ID`)
+    )
+    ENGINE=InnoDB " . $wpdb->get_charset_collate() . " AUTO_INCREMENT=1;";
+
+    require_once( ABSPATH . '/wp-admin/includes/upgrade.php');
+    dbDelta( $createSQL );
 }
  ?>
